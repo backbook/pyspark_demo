74870f0190fff309466005a6b39c3c331d109d0e
su <su>
2018-08-24 16:19:00 +0800

    Initial commit

---
+++ word_count.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+
+from pyspark.sql import SparkSession
+
+
+if __name__ == "__main__":
+    """
+        Usage: pi [partitions]
+    """
+    spark = SparkSession\
+        .builder\
+        .appName("PythonPi")\
+        .getOrCreate()
+    spark.sparkContext.setLogLevel("WARN")
+    text_file = spark.sparkContext.textFile("E:\\redhaat.txt")
+    counts = text_file.flatMap(lambda line: line.split(" ")) \
+        .map(lambda word: (word, 1)) \
+        .reduceByKey(lambda a, b: a + b)
+    print(counts.collect())

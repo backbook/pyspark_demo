6562d12c93f2afd3f03599f2c19362f353f8825a
backbook <76886049@qq.com>
2018-11-01 11:47:16 +0800

    init

--- sparkML_test/basic_statistics_RDD_based_API/PearsonBeijng.py
+++ sparkML_test/basic_statistics_RDD_based_API/PearsonBeijng.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 #coding=utf-8
 
+import pymysql
+
 
 from pyspark.sql import SparkSession
 # import numpy as np
@@ -26,3 +28,30 @@ precipitationRDD = flatRDD.filter(lambda value : float(value) < 1000)
 #皮尔逊相关系数验证
 PearsonBeijngStat = Statistics.corr(yearRDD,precipitationRDD)
 print(PearsonBeijngStat)
+
+# 创建连接
+conn = pymysql.connect(host="127.0.0.1", port=3306, user='root', passwd='Server2008!', db='spark', charset='utf8mb4')
+print(conn)
+
+# 创建游标(查询数据返回为元组格式)
+# cursor = conn.cursor()
+
+# 创建游标(查询数据返回为字典格式)
+cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+# 1. 执行SQL,返回受影响的行数
+effect_row1 = cursor.execute("select * from day_video_city_access_topn_stat")
+
+#获取第一行的数据
+row_1 = cursor.fetchone()
+
+#获取前n行的数据
+row_2 = cursor.fetchmany(3)
+
+row_3 = cursor.fetchall()
+
+print(effect_row1)
+
+print(row_1)
+print(row_2)
+print(row_3)
